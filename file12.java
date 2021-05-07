8ef9b8734582a7827a101b5bf99ef3ab117a2556
AKSHAY-MORE18 <83824440+AKSHAY-MORE18@users.noreply.github.com>
2021-05-07 22:42:44 +0530

    Add files via upload

---
+++ file12.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class file12
+{
+public static void main(String args[])
+{ 
+Scanner sc =new Scanner (System.in);
+
+System.out.println("Enter 1st no ;   ");
+
+int num1 = sc.nextInt();
+
+System.out.println("Enter 2nd no ;   ");
+
+int num2 = sc.nextInt();
+
+System.out.println("Enter 3rd no ;   ");
+
+int num3 = sc.nextInt();
+
+System.out.println( "the avarage of 1st three numbes is :   "  +(num1 + num2+ num3)/5);
+}
+}
+
---
+++ file15.java
@@ -0,0 +1,22 @@
+public class file15
+{
+	public static void main(String args[])
+	{
+		int x=100, y=200;
+		System.out.println("Before sswap :  ");
+		System.out.println(" x= " +x);
+		System.out.println("  y=  "+y);
+		int temp=x;
+		x=y;
+		y=temp;
+		System.out.println("after sswap :  ");
+		System.out.println(" x= " +x);
+		System.out.println("  y=  "+y);
+		
+	}
+}
+
+		
+		
+		
+		
